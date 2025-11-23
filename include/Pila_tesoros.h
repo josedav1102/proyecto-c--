d33950390b746ca1da275020ef1aca5adb1cd790
Julian Andres <juli.hurtado1407@gmail.com>
2025-11-23 17:31:27 -0500

    fix: agregar include de Treasure.h en Pila_tesoros

---
+++ include/Pila_tesoros.h
@@ -0,0 +1,21 @@
+//
+// Created by Freddy Hurtado on 11/23/25.
+//
+
+#ifndef JUEGOLABERINTO_PILA_TESOROS_H
+#define JUEGOLABERINTO_PILA_TESOROS_H
+
+#include <stack>
+#include <Treasure.h>
+class PilaTesoros {
+private:
+    std::stack<Treasure> stack;
+    public:
+    void push(const Treasure& t);
+    Treasure pop();
+    Treasure top() const;
+    bool empty() const;
+    int size() const;
+};
+
+#endif //JUEGOLABERINTO_PILA_TESOROS_H
